315c0d5bf6e0db25b5864e16b206f2f0d3d172ba
heleven-pc <heleven-pc@heleven-pc>
2021-11-17 21:32:26 +0100

    hashCode y equals de Posicion

--- src/main/java/org/iesalandalus/programacion/torreajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/torreajedrez/Posicion.java
@@ -1,5 +1,7 @@
 package org.iesalandalus.programacion.torreajedrez;
 
+import java.util.Objects;
+
 public class Posicion {
 	
 	// Declarar atributos
@@ -43,6 +45,25 @@ public class Posicion {
 	public Posicion(Posicion posicionOrigen) {
 		setFila(posicionOrigen.getFila());
 		setColumna(posicionOrigen.getColumna());
+	}
+
+	// hashCode y equals
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(columna, fila);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Posicion other = (Posicion) obj;
+		return columna == other.columna && fila == other.fila;
 	}
 	
 }
