95c983aca6fe52a65f5edc8b77266a30712ea5ac
heleven-pc <heleven-pc@192.168.22.11>
2021-11-20 13:22:54 +0100

    Getters y Setters Torre

--- src/main/java/org/iesalandalus/programacion/torreajedrez/Torre.java
+++ src/main/java/org/iesalandalus/programacion/torreajedrez/Torre.java
@@ -5,5 +5,31 @@ public class Torre {
 	private Color color;
 	private Posicion posicion;
 	
+	// Getter y setters
+	public Color getColor() {
+		return color;
+	}
+	
+	public void setColor(Color color) {
+		if (color==null)
+			throw new NullPointerException("ERROR: No puede haber un color nulo???????");
+		
+		if (color!=Color.BLANCO && color!=Color.NEGRO)
+			throw new IllegalArgumentException("ERROR: Color no valido");
+		
+		this.color = color;
+	}
+	
+	public Posicion getPosicion() {
+		return posicion;
+	}
+	
+	public void setPosicion(Posicion posicion) {
+		// Nuevo objeto haciendo uso del constructor copia
+		this.posicion = new Posicion(posicion);
+	}
+	
+	
+	
 	
 }
