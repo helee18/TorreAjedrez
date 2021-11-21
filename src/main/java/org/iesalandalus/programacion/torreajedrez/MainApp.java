3a0b4c50f14c57a72a0b16d94d772be1167ae569
heleven-pc <heleven-pc@heleven-pc>
2021-11-21 14:29:55 +0100

    Metodo mostrarTorre

--- src/main/java/org/iesalandalus/programacion/torreajedrez/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/torreajedrez/MainApp.java
@@ -2,8 +2,19 @@ package org.iesalandalus.programacion.torreajedrez;
 
 public class MainApp {
 
+	private static Torre torre;
+	
 	public static void main(String[] args) {
 		System.out.println("kk");
+		mostrarTorre();
+	}
+	
+	public static void mostrarTorre(){
+		try {
+			torre.toString();
+		}catch (NullPointerException e) {
+			System.out.println("ERROR: La torre no existe.");
+		}
 	}
 
 }
