ea7b5b65aadc6ca93941382a0a6491258e14f3be
heleven-pc <heleven-pc@heleven-pc>
2021-11-21 14:43:49 +0100

    metodo elegir opcion

--- src/main/java/org/iesalandalus/programacion/torreajedrez/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/torreajedrez/MainApp.java
@@ -1,8 +1,11 @@
 package org.iesalandalus.programacion.torreajedrez;
 
+import org.iesalandalus.programacion.utilidades.Entrada;
+
 public class MainApp {
 
 	private static Torre torre;
+	private static int opcion;
 	
 	public static void main(String[] args) {
 		System.out.println("kk");
@@ -24,5 +27,16 @@ public class MainApp {
 		System.out.println("3.- Crear torre de un color en una columna inicial dada ('c' o 'f')");
 		System.out.println("4.- Mover torre");
 		System.out.println("5.- Salir");
+	}
+	
+	public static int elegirOpcion() {
+		
+		do {
+			mostrarMenu();
+			System.out.println("Elige una opción");
+			opcion = Entrada.entero();
+		} while (opcion != 1 && opcion != 2 && opcion != 3 && opcion != 4 && opcion != 5);
+		
+		return opcion;
 	}
 }
